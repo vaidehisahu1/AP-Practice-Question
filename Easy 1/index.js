40d272213e7f9a22ca9de447e50fa88151abafdf
VAIDEHI SAHU <vaidehisahunst@gmail.com>
2025-12-04 17:02:22 +0530

    Update index.js

--- Easy 1/index.js
+++ Easy 1/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const path = require("path");
 
 function writeDataFile() {
-
+  const filePath = path.join(__dirname, "file.txt");  // absolute path
+  fs.writeFileSync(filePath, "We will not miss the exam next time"); 
 }
